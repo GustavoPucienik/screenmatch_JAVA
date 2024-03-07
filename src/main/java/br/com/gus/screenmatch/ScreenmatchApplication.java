e59c6a94140dcc1ff1abdaff7eaff06ceba14c59
GustavoPucienik <gomespucienik@gmail.com>
2024-03-06 22:27:46 -0300

    Iniciando api spring

---
+++ src/main/java/br/com/gus/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,26 @@
+package br.com.gus.screenmatch;
+
+import br.com.gus.screenmatch.model.DadosSerie;
+import br.com.gus.screenmatch.service.ConsumoApi;
+import br.com.gus.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=3bf61f2b");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}
