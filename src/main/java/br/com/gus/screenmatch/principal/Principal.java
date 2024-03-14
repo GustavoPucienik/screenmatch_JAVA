9ceb1fe4a33160ec785e04b36c15f4e597ffe228
GustavoPucienik <gomespucienik@gmail.com>
2024-03-14 20:48:30 -0300

    Aprendendo sobre peek, stream, Optional e Map

--- src/main/java/br/com/gus/screenmatch/principal/Principal.java
+++ src/main/java/br/com/gus/screenmatch/principal/Principal.java
@@ -3,12 +3,14 @@ package br.com.gus.screenmatch.principal;
 import br.com.gus.screenmatch.model.DadosEpisodio;
 import br.com.gus.screenmatch.model.DadosSerie;
 import br.com.gus.screenmatch.model.DadosTemporada;
+import br.com.gus.screenmatch.model.Episodio;
 import br.com.gus.screenmatch.service.ConsumoApi;
 import br.com.gus.screenmatch.service.ConverteDados;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Principal {
   private Scanner leitura = new Scanner(System.in);
@@ -42,6 +44,64 @@ public class Principal {
 
     temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
     temporadas.forEach(System.out::println);
+
+    List<DadosEpisodio> dadosEpisodios = temporadas.stream()
+        .flatMap(t -> t.episodios().stream())
+        .collect(Collectors.toList());
+
+//    System.out.println("\nTop 5 episódios");
+//    dadosEpisodios.stream()
+//        .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+//        .peek(e -> System.out.println("Primeiro filtro(N/A) " + e))
+//        .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+//        .peek(e -> System.out.println("Ordenação " + e))
+//        .limit(5)
+//        .peek(e -> System.out.println("Limite " + e))
+//        .map(e -> e.titulo().toUpperCase())
+//        .peek(e -> System.out.println("Mapeamento " + e))
+//        .forEach(System.out::println);
+
+    List<Episodio> episodios = temporadas.stream()
+        .flatMap(t -> t.episodios().stream()
+            .map(d -> new Episodio(t.numero(), d)))
+        .collect(Collectors.toList());
+
+    episodios.forEach(System.out::println);
+
+//    System.out.println("Digite um trecho do titulo do episódio");
+//    var trechoTitulo = leitura.nextLine();
+//    Optional<Episodio> episodioBuscado = episodios.stream()
+//        .filter(e -> e.getTitulo().toUpperCase().contains(trechoTitulo.toUpperCase() ))
+//        .findFirst();
+//    if (episodioBuscado.isPresent()) {
+//      System.out.println("Episódio encontrado!");
+//      System.out.println("Temporada: " + episodioBuscado.get().getTemporada());
+//    } else {
+//      System.out.println("Episódio não encontrado!");
+//    }
+
+//    System.out.println("A partir de que ano você deseja ver os episódios? ");
+//    var ano = leitura.nextInt();
+//    leitura.nextLine();
+//
+//    LocalDate dataBusca = LocalDate.of(ano, 1, 1);
+//
+//    DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+//
+//    episodios.stream()
+//        .filter(e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(dataBusca))
+//        .forEach(e -> System.out.println(
+//            "Temporada:  " + e.getTemporada() +
+//                " Episódio: " + e.getTitulo() +
+//                " Data lançamento: " + e.getDataLancamento().format(formatador)
+//        ));
+    
+    Map<Integer, Double> avaliacoesPorTemporada = episodios.stream()
+        .filter(e -> e.getAvaliacao() > 0.0)
+        .collect(Collectors.groupingBy(Episodio::getTemporada,
+            Collectors.averagingDouble(Episodio::getAvaliacao)));
+    System.out.println(avaliacoesPorTemporada);
+
 
   }
 }
